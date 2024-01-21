85fbb74aa894482a616b44e2b1ddcec9b2c579f5
ROB0-Head <maxim.fonariuc@gmail.com>
2024-01-21 20:52:06 +0700

    Правки по текстам

--- Assets/Scripts/UI/Screens/SlideshowScreen.cs
+++ Assets/Scripts/UI/Screens/SlideshowScreen.cs
@@ -16,11 +16,12 @@ namespace UI.Screens
         [SerializeField] private List<string> _narratorTexts;
 
 
-        private int currentIndex;
-        private List<Vector3> leftPhotosOriginalPositions = new List<Vector3>();
-        private List<Vector3> rightPhotosOriginalPositions = new List<Vector3>();
+        private int _currentIndex;
+        private List<Vector3> _leftPhotosOriginalPositions = new List<Vector3>();
+        private List<Vector3> _rightPhotosOriginalPositions = new List<Vector3>();
+        private bool _isSlideshowFinished;
 
-        public bool IsSlideshowFinished;
+        public bool IsSlideshowFinished => _isSlideshowFinished;
 
         public void Start()
         {
@@ -32,12 +33,12 @@ namespace UI.Screens
         {
             for (int i = 0; i < _leftPhotos.Count; i++)
             {
-                leftPhotosOriginalPositions.Add(_leftPhotos[i].gameObject.transform.localPosition);
+                _leftPhotosOriginalPositions.Add(_leftPhotos[i].gameObject.transform.localPosition);
             }
 
             for (int i = 0; i < _rightPhotos.Count; i++)
             {
-                rightPhotosOriginalPositions.Add(_rightPhotos[i].gameObject.transform.localPosition);
+                _rightPhotosOriginalPositions.Add(_rightPhotos[i].gameObject.transform.localPosition);
             }
         }
 
@@ -45,16 +46,16 @@ namespace UI.Screens
         {
             yield return MovePhotosOntoCanvas();
 
-            while (currentIndex < _narratorTexts.Count)
+            while (_currentIndex < _narratorTexts.Count)
             {
-                yield return TypeNarratorText(_narratorTexts[currentIndex]);
+                yield return TypeNarratorText(_narratorTexts[_currentIndex]);
 
-                yield return MovePhotosOutOfCanvas(currentIndex);
+                yield return MovePhotosOutOfCanvas(_currentIndex);
 
-                currentIndex++;
+                _currentIndex++;
             }
 
-            IsSlideshowFinished = true;
+            _isSlideshowFinished = true;
             Debug.Log("Slideshow finished!");
         }
 
@@ -63,13 +64,13 @@ namespace UI.Screens
             for (int i = 0; i < _leftPhotos.Count; i++)
             {
                 _leftPhotos[i].gameObject.transform.localPosition =
-                    new Vector3(-3000f, leftPhotosOriginalPositions[i].y, 0);
+                    new Vector3(-3000f, _leftPhotosOriginalPositions[i].y, 0);
             }
 
             for (int i = 0; i < _rightPhotos.Count; i++)
             {
                 _rightPhotos[i].gameObject.transform.localPosition =
-                    new Vector3(3000f, rightPhotosOriginalPositions[i].y, 0);
+                    new Vector3(3000f, _rightPhotosOriginalPositions[i].y, 0);
             }
 
             yield return new WaitForSeconds(1f);
@@ -82,14 +83,14 @@ namespace UI.Screens
             if (_leftPhotos.Count - 1 >= index)
             {
                 mySequence.Append(_leftPhotos[index].gameObject.transform
-                    .DOLocalMoveX(leftPhotosOriginalPositions[index].x, 0.2f));
+                    .DOLocalMoveX(_leftPhotosOriginalPositions[index].x, 0.2f));
                 mySequence.Play();
             }
 
             if (_rightPhotos.Count - 1 >= index)
             {
                 mySequence.Append(_rightPhotos[index].gameObject.transform
-                    .DOLocalMoveX(rightPhotosOriginalPositions[index].x, 0.2f));
+                    .DOLocalMoveX(_rightPhotosOriginalPositions[index].x, 0.2f));
                 mySequence.Play();
             }
 
